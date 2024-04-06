d7a154fde336930ac85facd402ae11e203561265
Naveed Ahmed <160510766+Naveedians@users.noreply.github.com>
2024-04-06 18:24:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.85,
+    PKR: 280,
+};
+let user_answer = await inquirer.prompt([{
+        name: 'from',
+        message: 'Enter from currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'to',
+        message: 'Enter to currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'amount',
+        message: 'Enter amount',
+        type: 'number'
+    }
+]);
+let fromAmount = currency[user_answer.from]; //exchange rate
+let toAmount = currency[user_answer.to]; //exchange rate
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD base currency 
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
